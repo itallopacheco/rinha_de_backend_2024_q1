5c21602ff85893a32b6269ee78f6f74bc0b528f3
= <=>
2024-02-26 21:22:05 -0300

    add TODO

--- rinha_capivara/app.py
+++ rinha_capivara/app.py
@@ -27,6 +27,20 @@ def get_clientes(session: Session, skip: int = 0, limit: int = 100):
 
 @app.post('/clientes/{cliente_id}/transacoes')
 def fazer_transacao(cliente_id: int, transacao: TransacaoSchema, session: Session):
+    #TODO: verificar se o paciente pode fazer a transacao
+
+    """
+    Regras Uma transação de débito nunca pode deixar o saldo do cliente menor que seu limite disponível.
+    Por exemplo, um cliente com limite de 1000 (R$ 10) nunca deverá ter o saldo menor que -1000 (R$ -10).
+     Nesse caso, um saldo de -1001 ou menor significa inconsistência na Rinha de Backend!
+
+     # Se uma requisição para débito for deixar o saldo inconsistente, a API deve retornar HTTP Status Code 422
+     sem completar a transação! O corpo da resposta nesse caso não será testado e você pode escolher como o representar.
+
+     Se o atributo [id] da URL for de uma identificação não existente de cliente, a API deve retornar HTTP Status Code 404.
+     O corpo da resposta nesse caso não será testado e você pode escolher como o representar.
+    """
+
     cliente = session.scalar(select(Cliente).where(Cliente.id == cliente_id))
 
     if not cliente:
